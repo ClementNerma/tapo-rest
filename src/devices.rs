730821bd730801b67fdd9cab70c0ddf46cf9aef0
Clément Nerma <clement.nerma@gmail.com>
2024-04-21 14:32:40 +0200

    Update dependencies

--- src/devices.rs
+++ src/devices.rs
@@ -1,4 +1,4 @@
-use anyhow::{Context, Result};
+use anyhow::{anyhow, Result};
 use tapo::{
     ApiClient, ColorLightHandler, ColorLightStripHandler, LightHandler,
     PlugEnergyMonitoringHandler, PlugHandler,
@@ -42,109 +42,98 @@ impl TapoDevice {
 
         let TapoCredentials { username, password } = credentials;
 
-        let tapo_client = ApiClient::new(username, password)
-            .with_context(|| format!("Failed to connect to Tapo device '{name}'"))?;
+        let tapo_client = ApiClient::new(username, password);
 
-        let inner = match device_type {
-            TapoDeviceType::L510 => {
-                let auth = tapo_client
-                    .l510(ip_addr.to_string())
-                    .await
-                    .with_context(|| format!("Failed to login into L510 bulb '{name}'"))?;
+        let inner =
+            match device_type {
+                TapoDeviceType::L510 => {
+                    let auth = tapo_client.l510(ip_addr.to_string()).await.map_err(|err| {
+                        anyhow!("Failed to login into L510 bulb '{name}': {err:?}")
+                    })?;
 
-                TapoDeviceInner::L510(auth)
-            }
+                    TapoDeviceInner::L510(auth)
+                }
 
-            TapoDeviceType::L530 => {
-                let auth = tapo_client
-                    .l530(ip_addr.to_string())
-                    .await
-                    .with_context(|| format!("Failed to login into L530 bulb '{name}'"))?;
+                TapoDeviceType::L530 => {
+                    let auth = tapo_client.l530(ip_addr.to_string()).await.map_err(|err| {
+                        anyhow!("Failed to login into L530 bulb '{name}': {err:?}")
+                    })?;
 
-                TapoDeviceInner::L530(auth)
-            }
+                    TapoDeviceInner::L530(auth)
+                }
 
-            TapoDeviceType::L610 => {
-                let auth = tapo_client
-                    .l610(ip_addr.to_string())
-                    .await
-                    .with_context(|| format!("Failed to login into L610 bulb '{name}'"))?;
+                TapoDeviceType::L610 => {
+                    let auth = tapo_client.l610(ip_addr.to_string()).await.map_err(|err| {
+                        anyhow!("Failed to login into L610 bulb '{name}': {err:?}")
+                    })?;
 
-                TapoDeviceInner::L610(auth)
-            }
+                    TapoDeviceInner::L610(auth)
+                }
 
-            TapoDeviceType::L630 => {
-                let auth = tapo_client
-                    .l630(ip_addr.to_string())
-                    .await
-                    .with_context(|| format!("Failed to login into L630 bulb '{name}'"))?;
+                TapoDeviceType::L630 => {
+                    let auth = tapo_client.l630(ip_addr.to_string()).await.map_err(|err| {
+                        anyhow!("Failed to login into L630 bulb '{name}': {err:?}")
+                    })?;
 
-                TapoDeviceInner::L630(auth)
-            }
+                    TapoDeviceInner::L630(auth)
+                }
 
-            TapoDeviceType::L900 => {
-                let auth = tapo_client
-                    .l900(ip_addr.to_string())
-                    .await
-                    .with_context(|| format!("Failed to login into L900 bulb '{name}'"))?;
+                TapoDeviceType::L900 => {
+                    let auth = tapo_client.l900(ip_addr.to_string()).await.map_err(|err| {
+                        anyhow!("Failed to login into L900 bulb '{name}': {err:?}")
+                    })?;
 
-                TapoDeviceInner::L900(auth)
-            }
+                    TapoDeviceInner::L900(auth)
+                }
 
-            TapoDeviceType::L920 => {
-                let auth = tapo_client
-                    .l920(ip_addr.to_string())
-                    .await
-                    .with_context(|| format!("Failed to login into L920 strip '{name}'"))?;
+                TapoDeviceType::L920 => {
+                    let auth = tapo_client.l920(ip_addr.to_string()).await.map_err(|err| {
+                        anyhow!("Failed to login into L920 strip '{name}': {err:?}")
+                    })?;
 
-                TapoDeviceInner::L920(auth)
-            }
+                    TapoDeviceInner::L920(auth)
+                }
 
-            TapoDeviceType::L930 => {
-                let auth = tapo_client
-                    .l930(ip_addr.to_string())
-                    .await
-                    .with_context(|| format!("Failed to login into L930 strip '{name}'"))?;
+                TapoDeviceType::L930 => {
+                    let auth = tapo_client.l930(ip_addr.to_string()).await.map_err(|err| {
+                        anyhow!("Failed to login into L930 strip '{name}': {err:?}")
+                    })?;
 
-                TapoDeviceInner::L930(auth)
-            }
+                    TapoDeviceInner::L930(auth)
+                }
 
-            TapoDeviceType::P100 => {
-                let auth = tapo_client
-                    .p100(ip_addr.to_string())
-                    .await
-                    .with_context(|| format!("Failed to login into P100 plug '{name}'"))?;
+                TapoDeviceType::P100 => {
+                    let auth = tapo_client.p100(ip_addr.to_string()).await.map_err(|err| {
+                        anyhow!("Failed to login into P100 plug '{name}': {err:?}")
+                    })?;
 
-                TapoDeviceInner::P100(auth)
-            }
+                    TapoDeviceInner::P100(auth)
+                }
 
-            TapoDeviceType::P105 => {
-                let auth = tapo_client
-                    .p105(ip_addr.to_string())
-                    .await
-                    .with_context(|| format!("Failed to login into P105 plug '{name}'"))?;
+                TapoDeviceType::P105 => {
+                    let auth = tapo_client.p105(ip_addr.to_string()).await.map_err(|err| {
+                        anyhow!("Failed to login into P105 plug '{name}': {err:?}")
+                    })?;
 
-                TapoDeviceInner::P105(auth)
-            }
+                    TapoDeviceInner::P105(auth)
+                }
 
-            TapoDeviceType::P110 => {
-                let auth = tapo_client
-                    .p110(ip_addr.to_string())
-                    .await
-                    .with_context(|| format!("Failed to login into P110 plug '{name}'"))?;
+                TapoDeviceType::P110 => {
+                    let auth = tapo_client.p110(ip_addr.to_string()).await.map_err(|err| {
+                        anyhow!("Failed to login into P110 plug '{name}': {err:?}")
+                    })?;
 
-                TapoDeviceInner::P110(auth)
-            }
+                    TapoDeviceInner::P110(auth)
+                }
 
-            TapoDeviceType::P115 => {
-                let auth = tapo_client
-                    .p115(ip_addr.to_string())
-                    .await
-                    .with_context(|| format!("Failed to login into P115 plug '{name}'"))?;
+                TapoDeviceType::P115 => {
+                    let auth = tapo_client.p115(ip_addr.to_string()).await.map_err(|err| {
+                        anyhow!("Failed to login into P115 plug '{name}': {err:?}")
+                    })?;
 
-                TapoDeviceInner::P115(auth)
-            }
-        };
+                    TapoDeviceInner::P115(auth)
+                }
+            };
 
         Ok(Self {
             name: name.to_string(),
